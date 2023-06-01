960c873bb701c9a4629f55995343e9df6c432f47
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2023-06-01 13:08:43 +0000

    Created Sidebar component

---
+++ components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <aside class="sidebar">
+    <!-- Conteúdo da sidebar -->
+  </aside>
+</template>
+
+<style scoped>
+.sidebar {
+  background-color: purple; /* Cor principal roxa */
+  width: 250px;
+  /* Estilos adicionais para a sidebar */
+}
+</style>
