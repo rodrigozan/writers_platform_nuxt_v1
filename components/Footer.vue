dfb2b1ac87f028d0363cec56272c0bbe9855ac3b
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2023-06-01 13:08:13 +0000

    Created Footer and Header components

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <!-- Conteúdo do footer -->
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: purple; /* Cor principal roxa */
+  height: 60px;
+  /* Estilos adicionais para o footer */
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <!-- Conteúdo do header -->
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background-color: purple; /* Cor principal roxa */
+  height: 60px;
+  /* Estilos adicionais para o header */
+}
+</style>
