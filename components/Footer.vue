56faf0a5b1ecd53a834302e43d0400ae2b825bc4
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2023-06-01 13:15:28 +0000

    Updated footer component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,35 @@
 <template>
-  <footer class="footer">
-    <!-- Conteúdo do footer -->
-  </footer>
+  <v-footer class="d-flex flex-column">
+    <div class="bg-teal d-flex w-100 align-center px-4">
+      <strong>Get connected with us on social networks!</strong>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="plain"
+        size="small"
+      ></v-btn>
+    </div>
+
+    <div class="px-4 py-2 bg-black text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </div>
+  </v-footer>
 </template>
 
-<style scoped>
-.footer {
-  background-color: purple; /* Cor principal roxa */
-  height: 60px;
-  /* Estilos adicionais para o footer */
-}
-</style>
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
+</script>
