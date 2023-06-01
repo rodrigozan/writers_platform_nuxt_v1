77469a878646cc7ddcb76261766618c736cda7c1
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2023-06-01 19:33:23 +0000

    Created components and default layout

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="container-fluid">
+  Footer
+  </footer>
+</template>
+
+<script>
+export default {
+  methods: {},
+};
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="container-fluid">
+  Header
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {},
+};
+</script>
