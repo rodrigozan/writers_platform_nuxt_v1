6136eb5849ba808b80b1c90c64ef71a73f27a813
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2023-06-01 13:10:43 +0000

    Created Default layout

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="layout container-fluid">
+    <Header />
+    <main class="main">
+      <Sidebar />
+      <nuxt />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<style>
+.layout {
+  display: flex;
+  flex-direction: column;
+  /* Estilos adicionais para o layout padrão */
+}
+
+.main {
+  display: flex;
+  flex-direction: row;
+  /* Estilos adicionais para o conteúdo principal */
+}
+</style>
+
+<script>
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+import Sidebar from '~/components/Sidebar.vue';
+
+export default {
+  components: {
+    Header,
+    Footer,
+    Sidebar,
+  },
+};
+</script>
